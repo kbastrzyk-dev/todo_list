02fa9b80be00eb09606093aa3a33c3c7bef15c91
kbastrzyk-dev <kris.bastrzyk@gmail.com>
2026-08-20 12:45:22 +0200

    feat: add ExerciseService and extract database logic into in

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Exercise;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\ExerciseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ExerciseController extends AbstractController
 {
     #[Route('/exercise', name: 'app_exercise')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, ExerciseService $exerciseService): Response
     {   
         // Sprawdzamy czy ktoś kliknał przycisk "Dodaj ćwiczenie" w formularzu
         if ($request->isMethod('POST')) {
@@ -20,23 +19,16 @@ final class ExerciseController extends AbstractController
             $reps = $request->request->get('reps');
 
             if ($name && $reps) {
-                // Nowy obiekt Exercise
-                $exercise = new Exercise();
-                $exercise->setName($name);
-                $exercise->setReps((int) $reps);
-                $exercise->setIsDone(false);
-
-                // Zapisujemy ćwiczenie do bazy danych
-                $entityManager->persist($exercise);
-                $entityManager->flush();
+                // teraz ExerciseService zajmie się dodaniem ćwiczenia do bazy danych
+                $exerciseService->createExercise($name, (int)$reps);
 
                 // Odświeżamy stronę, żeby formularz się wyczyścił
                 return $this->redirectToRoute('app_exercise');
             }
         }
 
-        //Pobieramy wszystkie zapisane ćwiczenia z bazy
-        $exercises = $entityManager->getRepository(Exercise::class)->findAll();
+        //Pobieramy wszystkie zapisane ćwiczenia korzystając z ExerciseService
+        $exercises = $exerciseService->getAllExercises();
 
         // Przekazujemy ćwiczenia do pliku HTML
         return $this->render('exercise/index.html.twig', [
---
+++ src/Service/ExerciseService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Exercise;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ExerciseService
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function createExercise(string $name, int $reps): void
+    {
+        $exercise = new Exercise();
+        $exercise->setName($name);
+        $exercise->setReps($reps);
+        $exercise->setIsDone(false);
+
+        $this->entityManager->persist($exercise);
+        $this->entityManager->flush();
+    }
+
+    public function getAllExercises(): array
+    {
+        return $this->entityManager->getRepository(Exercise::class)->findAll();
+    }
+}
