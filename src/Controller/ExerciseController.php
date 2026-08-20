9cf3a4413a34b4e3301c5fe70762cd357d134661
kbastrzyk-dev <kris.bastrzyk@gmail.com>
2026-08-20 12:25:32 +0200

    feat: add create and read for exercises

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Exercise;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ExerciseController extends AbstractController
 {
     #[Route('/exercise', name: 'app_exercise')]
-    public function index(): Response
-    {
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {   
+        // Sprawdzamy czy ktoś kliknał przycisk "Dodaj ćwiczenie" w formularzu
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            $reps = $request->request->get('reps');
+
+            if ($name && $reps) {
+                // Nowy obiekt Exercise
+                $exercise = new Exercise();
+                $exercise->setName($name);
+                $exercise->setReps((int) $reps);
+                $exercise->setIsDone(false);
+
+                // Zapisujemy ćwiczenie do bazy danych
+                $entityManager->persist($exercise);
+                $entityManager->flush();
+
+                // Odświeżamy stronę, żeby formularz się wyczyścił
+                return $this->redirectToRoute('app_exercise');
+            }
+        }
+
+        //Pobieramy wszystkie zapisane ćwiczenia z bazy
+        $exercises = $entityManager->getRepository(Exercise::class)->findAll();
+
+        // Przekazujemy ćwiczenia do pliku HTML
         return $this->render('exercise/index.html.twig', [
-            'exercises' => [],
+            'exercises' => $exercises,
         ]);
     }
 }
